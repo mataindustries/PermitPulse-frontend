4b1fee37dadc63e0050d682a92a5514e783e98e5
mataindustries <matasergio741@gmail.com>
2025-08-30 05:59:23 +0000

    modified:   src/pages/Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -134,11 +134,13 @@ export default function Home() {
             }}
           />
         <div className="mt-4 flex justify-center">
-          <button 
-            onClick={() => setWaitlistOpen(true)} 
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
-            Join Waitlist
-          </button>
+          <button
+            id="cta-waitlist"
+              onClick={() => setWaitlistOpen(true)}
+                className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+                >
+                  Join Waitlist
+                  </button>>
         </div>
         </div>
 
